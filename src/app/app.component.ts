6fd37f1a71fe27b954595da2770387426efd60ba
Leandro Ali Elel <Leandro.Ali.Elel@ar.ey.com>
2019-07-23 21:07:45 -0300

    project clean up

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,4 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
-import { debounceTime } from 'rxjs/operators';
-import { FormGroup, FormControl, FormBuilder, FormArray } from '@angular/forms';
-import { Validators } from '@angular/forms';
-import { Subscription } from 'rxjs';
-import { forbiddenNameValidator, samePasswordValidator } from './shared/directives/forbidden-names.directive';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -11,64 +6,4 @@ import { forbiddenNameValidator, samePasswordValidator } from './shared/directiv
   styleUrls: ['./app.component.css']
 })
 
-export class AppComponent implements OnInit, OnDestroy {
-
-  coffee = new FormControl('');
-  coffeeSubscription: Subscription;
-  profileForm = new FormGroup({
-    firstName: new FormControl(''),
-    lastName: new FormControl(''),
-  });
-
-  userLoginForm = this.fb.group({
-    password: [''],
-    newPassword: [''],
-    repeatedNewPassword: ['']
-  }, { validators: [samePasswordValidator] });
-
-  builderProfileForm = this.fb.group({
-    firstName: [''],
-    lastName: [''],
-    address: this.fb.group({
-      street: [''],
-      city: [''],
-      state: [''],
-      zip: ['']
-    }),
-    aliases: this.fb.array([
-      this.fb.control('', [forbiddenNameValidator([/bob/, /mike/])])
-    ])
-  });
-
-  constructor(private fb: FormBuilder) { }
-
-  ngOnInit() {
-    this.coffeeSubscription = this.coffee.valueChanges.pipe(
-      debounceTime(500)
-    ).subscribe(event => {
-      console.log(event);
-    });
-  }
-
-  onSubmit() {
-    console.log('submit!', this.profileForm.value);
-  }
-  onSubmitBuilder() {
-    console.log('submit builder!', this.builderProfileForm.value);
-  }
-
-  get aliases() {
-    return this.builderProfileForm.get('aliases') as FormArray;
-  }
-
-  addAlias() {
-    this.aliases.push(
-      this.fb.control('a', [forbiddenNameValidator([/bob/, /mike/])]
-      )
-    );
-  }
-
-  ngOnDestroy() {
-    this.coffeeSubscription.unsubscribe();
-  }
-}
+export class AppComponent  {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
-import { ForbiddenNamesDirective } from './shared/directives/forbidden-names.directive';
 import { AppRoutingModule } from './app-routing.module';
-// import { QuizSectionComponent } from './views/quiz/quiz-section/quiz-section.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    ForbiddenNamesDirective,
-    // QuizSectionComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    ReactiveFormsModule,
     AppRoutingModule
   ],
   providers: [],
